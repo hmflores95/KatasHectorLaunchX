5f81c060f7e0b50c4217a3d87c8f62270779e4b0
Hector Flores <hmflores95@gmail.com>
2022-02-21 01:08:26 -0700

    Se suben katas

---
+++ modulo1/convertidor.py
@@ -0,0 +1,5 @@
+parsec = 11
+
+lightyears = parsec * 3.26156
+
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
---
+++ modulo1/program.py
@@ -0,0 +1,32 @@
+# program.py
+sum = 1 + 2
+print(sum)
+
+# Impresión en pantalla print('Hola desde la consola')
+print('Hola desde la consola')
+
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+
+# Importamos la biblioteca 
+from datetime import date
+
+# Obtenemos la fecha de hoy
+date.today()
+
+# Mostramos la fecha en la consola
+print(date.today())
+
+print("Today's date is: " + str(date.today()))
+
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
+print("Calculadora")
+first_number = input("Primer número: ")
+second_number = input("Segundo número: ")
+print(first_number + second_number)
+
+print(int(first_number) + int(second_number))
